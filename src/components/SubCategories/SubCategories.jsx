660f5b57536ef48b4e28ea62b343c544435e32a1
Mohamed Elbana <88998148+Mo7amed-2lbana@users.noreply.github.com>
2023-07-20 17:44:28 +0300

    Update SubCategories.jsx

--- src/components/SubCategories/SubCategories.jsx
+++ src/components/SubCategories/SubCategories.jsx
@@ -3,12 +3,7 @@ import React from 'react'
 export default function SubCategories() {
   return<>
   
-  <div>ahnmmlk  </div>
-  <div>ahnmmlk  </div>
-  <div>ahnmmlk  </div>
-  <div>ahnmmlk  </div>
-  <div>ahnmmlk  </div>
-  <div>ahnmmlk  </div>
+  <div>Elbana</div>
   
   
   </>
